a5639c14f5a19e8ecefb303801e9db7a3ac0df30
Technochip <mitrat501@gmail.com>
2025-12-16 01:51:27 +0530

    fixing

--- shell.sh
+++ shell.sh
@@ -1,11 +1,4 @@
 echo "Test hello world"
-<<<<<<< HEAD
-echo "Lets test the workflow XD"
-echo "another test"
-
-
-=======
 echo "testing my push work X"
 
 SECRET = "AKIA123456789EXAMPLE";
->>>>>>> dev
